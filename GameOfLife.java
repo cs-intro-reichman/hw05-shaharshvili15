b852ce960da9d2cb4ad2a64b8c7f7c6e362c9349
shaharshvili15 <shahar.shvili@post.runi.ac.il>
2024-01-18 16:13:51 +0200

    add space

--- GameOfLife.java
+++ GameOfLife.java
@@ -168,7 +168,7 @@ public class GameOfLife {
 	public static void print(int[][] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
-				System.out.print(arr[i][j] + " ");
+				System.out.print(arr[i][j] + "  ");
 			}
 			System.out.println();
 		}
